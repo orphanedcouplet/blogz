824177d09e28e247e9345c203784ff05eb5e9c8e
orphanedcouplet <oliverdayers@gmail.com>
2017-10-13 15:42:08 -0500

    changed config for blogz db and updated class definitions to include datetime

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://build-a-blog:blog-a-build@localhost:8889/build-a-blog"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://blogz:blogzdevpassword@localhost:8889/blogz"
 db = SQLAlchemy(app)
 
-class Blog(db.Model):
 
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(280))
-    body = db.Column(db.String(10000000))
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+    category = db.relationship("Category", backref=db.backref("posts", lazy="dynamic"))
+
+    def __init__(self, title, body, category, pub_date=None):
         self.title = title
         self.body = body
+        if pub_date is None:
+            pub_date = datetime.now()
+        self.pub_date = pub_date
+        self.category = category
+    
+    def __repr__(self):
+        return "<Post %r>" % self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return "<Category %r>" % self.name
+
 
 @app.route("/blog", methods=["GET"])
 def index():
@@ -25,8 +49,6 @@ def index():
     entry_id = request.args.get("id")
 
     if entry_id:
-        # how do I access the title of the blog object already created <IN THE DATABASE> at id=[num]?
-        # <> : how do i query the database for id=[num] and related info?
         blog_entry = Blog.query.filter_by(id=entry_id).first()
         entry_title = blog_entry.title
         entry_body = blog_entry.body
@@ -36,18 +58,6 @@ def index():
     else:
         entries = Blog.query.order_by(desc(Blog.id)).all()
         return render_template("blog.html", title="The Blog", entries=entries)
-
-
-    # TODO handle GET request with query params
-    # (go to individual blog-entry template)
-
-    # URL format: ./blog?id=6
-    # GET request param: form_value = request.args.get("param_name")
-    # boils down to: /action_page?form_value={{request.args.get("param_name")}}
-    # so: /blog?id={{num}}
-    # id = request.args.get("num")
-
-
 
 
 @app.route("/newpost", methods=["POST", "GET"])
@@ -83,15 +93,7 @@ def new_post():
             entry_body_error=entry_body_error
             )
 
-
     return render_template("newpost.html")
-
-    #TODO fix the following bug:
-    # "return redirect('/blog')" >> can't see form to enter a new entry
-    # "return render_template('newpost.html')" >> doesn't redirect to /blog after posting entry
-    # probably needs a conditional?
-    
-
 
 
 if __name__ == "__main__":
